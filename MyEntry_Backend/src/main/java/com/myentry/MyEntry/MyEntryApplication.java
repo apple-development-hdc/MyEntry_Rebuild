7ecdeed599c1c47988b8e6516cb5ba0b47a83afa
sroy96 <sauravroy.eee@gmail.com>
2020-03-13 01:07:42 +0530

    backend-initialize

---
+++ MyEntry_Backend/src/main/java/com/myentry/MyEntry/MyEntryApplication.java
@@ -0,0 +1,13 @@
+package com.myentry.MyEntry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyEntryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyEntryApplication.class, args);
+	}
+
+}
---
+++ MyEntry_Backend/src/test/java/com/myentry/MyEntry/MyEntryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myentry.MyEntry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyEntryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
