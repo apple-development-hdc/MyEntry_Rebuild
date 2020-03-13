f4d66a1677ce1d98a4997bc9dae9d5d5e038dbb6
sroy96 <sauravroy.eee@gmail.com>
2020-03-14 02:19:58 +0530

    field edit

--- MyEntry_Backend/src/main/java/com/myentry/MyEntry/DTO/EmployeeDetails.java
+++ MyEntry_Backend/src/main/java/com/myentry/MyEntry/DTO/EmployeeDetails.java
@@ -7,4 +7,6 @@ import javax.imageio.ImageIO;
 
 public class EmployeeDetails {
     private static final long serialVersionUID = 1L;
+    private String employeeID;
+    private Locations employeeLocation;
 }
