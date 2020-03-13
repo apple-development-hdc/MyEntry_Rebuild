71f2abf253d32e28f7ed7fe19c17a7685bbd675d
sroy96 <sauravroy.eee@gmail.com>
2020-03-14 02:13:31 +0530

    partially working on EscortMangmnt

---
+++ MyEntry_Backend/src/main/java/com/myentry/MyEntry/DTO/EmployeeDetails.java
@@ -0,0 +1,10 @@
+package com.myentry.MyEntry.DTO;
+
+import com.myentry.MyEntry.Interfaces.EmployeeImageInterface;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.imageio.ImageIO;
+
+public class EmployeeDetails {
+    private static final long serialVersionUID = 1L;
+}
