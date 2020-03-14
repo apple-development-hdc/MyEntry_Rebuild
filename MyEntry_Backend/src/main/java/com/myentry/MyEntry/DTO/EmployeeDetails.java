98281502718137daa2c00a32c86c7917fd9523bf
sroy96 <sauravroy.eee@gmail.com>
2020-03-14 16:51:08 +0530

    Field Settings

--- MyEntry_Backend/src/main/java/com/myentry/MyEntry/DTO/EmployeeDetails.java
+++ MyEntry_Backend/src/main/java/com/myentry/MyEntry/DTO/EmployeeDetails.java
@@ -1,10 +1,5 @@
 package com.myentry.MyEntry.DTO;
 
-import com.myentry.MyEntry.Interfaces.EmployeeImageInterface;
-import org.springframework.web.multipart.MultipartFile;
-
-import javax.imageio.ImageIO;
-
 public class EmployeeDetails {
     private static final long serialVersionUID = 1L;
     private String employeeID;
