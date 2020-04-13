ab291dc1045e773bf1a259b0f8c0dfe816b9ba5e
himanshu0244 <42444625+himanshu0244@users.noreply.github.com>
2020-04-14 02:54:00 +0530

    added props

--- MyEntry_Backend/src/main/java/com/myentry/MyEntry/domain/BaseEntity.java
+++ MyEntry_Backend/src/main/java/com/myentry/MyEntry/domain/BaseEntity.java
@@ -1,34 +1,46 @@
 package com.myentry.MyEntry.domain;
 
-
-
-
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import java.time.LocalDateTime;
 
-import static com.myentry.MyEntry.constants.CommonConstants.LOGIN_DATE;
-import static com.myentry.MyEntry.constants.CommonConstants.LOGOUT_DATE;
+import static com.myentry.MyEntry.constants.CommonConstants.*;
 
 @Getter
 @Setter
 @MappedSuperclass
 public class BaseEntity {
-    @Column(name = LOGIN_DATE)
-    private LocalDateTime loginDate;
 
-    @Column(name = LOGOUT_DATE)
-    private LocalDateTime logoutDate;
+    @Column(name = CREATED_DATE)
+    private LocalDateTime createDate;
 
+    @Column(name = MODIFIED_DATE)
+    private LocalDateTime modifiedDate;
 
-    public void loginDateSetter() {
-        this.loginDate = LocalDateTime.now();
+    @Column(name = CREATED_BY)
+    private String createdBy;
+
+    @Column(name = MODIFIED_BY)
+    private String modifiedBy;
+
+    /**
+     * Stores created date in DB for new record
+     */
+    @PrePersist
+    public void prePersist() {
+        this.createDate = LocalDateTime.now();
+        this.createdBy = "SYS_ADMIN";
     }
-    public void logoutDateSetter() {
-        this.loginDate = LocalDateTime.now();
-    }
 
+    @PreUpdate
+    public void preUpdate() {
+        this.modifiedDate = LocalDateTime.now();
+        this.modifiedBy = "SYS_ADMIN";
+    }
 }
+
